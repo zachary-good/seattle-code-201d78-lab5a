1c3218d00e4f0f22747c566993e2348d940fcc1b
Zachary Good <zjgood13@gmail.com>
2021-08-13 16:01:44 -0700

    problem four completed

--- starter-code/app.js
+++ starter-code/app.js
@@ -98,12 +98,19 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 
 function sumArray(sumArr) { //eslint-disable-line
- 
+  let sumProb4a = sum(sumArr[0], sumArr[1]);
+  let sumProb4b = sum(sumProb4a[0], sumArr[2]);
+
+  let prob4String = sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + sumProb4b[0] + ' is their sum.'
+
+  return[sumProb4b[0], prob4String];
+
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
- //testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
